51ae1b165dec0335dfa9d10e960d2b5318f6b440
1096samiyajannat-source <1096.samiyajannat@gmail.com>
2026-07-28 08:31:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-// Kawser FF Topup V3
-// Foundation Script
-
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Kawser FF Topup Loaded Successfully!");
 
-    const startBtn = document.querySelector(".start-btn");
+  const buttons = document.querySelectorAll("button");
 
-    if (startBtn) {
-        startBtn.addEventListener("click", function () {
-            alert("🚀 Welcome to Kawser FF Topup!\n\nOrder System Coming Soon.");
-        });
+  buttons.forEach(function(btn){
+
+    if(btn.innerText.includes("BUY NOW")){
+
+      btn.addEventListener("click", function(){
+
+        let offer = this.parentElement.querySelector("h3").innerText;
+
+        let uid = prompt("Enter Your Free Fire UID");
+
+        if(!uid) return;
+
+        let trx = prompt(
+`Selected Offer:
+${offer}
+
+bKash: 01614156718
+Nagad: 01742138790
+
+Send Money করে Transaction ID লিখুন`
+);
+
+        if(!trx) return;
+
+        alert(
+`✅ Order Submitted
+
+Offer: ${offer}
+UID: ${uid}
+Transaction ID: ${trx}
+
+⚠️ যদি নির্ধারিত টাকার থেকে কম Send Money করেন তাহলে আপনার Order Cancel হতে পারে।
+
+আপনার Order খুব দ্রুত Check করা হবে।`
+);
+
+      });
+
     }
+
+  });
+
 });
